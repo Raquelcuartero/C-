fef4e13db45b93d11e9b94fceb6f13b0961ade30
Raquelcuartero <33655363+Raquelcuartero@users.noreply.github.com>
2017-12-19 11:16:09 +0100

    Add files via upload

--- fichero2.cpp
+++ fichero2.cpp
@@ -1,14 +1,20 @@
+//leer lineas
 #include<iostream>
 #include<fstream>
 
 using namespace std;
+      
 int main(){
+    ifstream fichero("mi bonito fichero.txt");
     string mensaje;
     int salir;
-    ifstream fichero("mi bonito fichero.txt");
-    fichero>>mensaje;
-    cout<<mensaje<<endl;
-    fichero.close(); //no es obligatorio esto
+    cout<<"El texto dice: "<<endl;
+    while(fichero.eof()!=1{
+       getline(fichero,mensaje);
+       if fichero
+       cout<< mensaje;
+       cout<<endl;
+    }
     cin>>salir;
     return 0;
-}
+    }
