fabcd33c38fa88254e2ebc4504e38e6abe1c9b8f
Raquelcuartero <33655363+Raquelcuartero@users.noreply.github.com>
2018-01-11 11:27:50 +0100

    Add files via upload

---
+++ ficheroeof.cpp
@@ -0,0 +1,21 @@
+//lectura hasta el final
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+    string linea;
+    int salir;
+    ifstream fichero("Tufichero.txt");
+    if(fichero.fail()==1){
+       cout<<"ERROR.No existe la ruta";
+       cin>>salir;
+       exit(1);
+    }
+    while(fichero.eof()==0){
+      getline(fichero,linea);
+      cout<<linea<<endl;
+    }
+    fichero.close();
+    cin>>salir;
+    return 0;
+}
