a9919cedfe0d0023ce34ee51f60444cacf9456ef
Raquelcuartero <33655363+Raquelcuartero@users.noreply.github.com>
2018-01-23 11:54:06 +0100

    Add files via upload

---
+++ puntero_4.cpp
@@ -0,0 +1,12 @@
+//Programas sobre el manejo de memoria
+//Vamos a declarar 2 variables a y b y guardarlas en &a que es
+#include<stdio.h>
+
+int main(){
+    int a=3;
+    int b=2;
+    int salir;
+    printf("direccion de a= %x\n",&a);
+    printf("direccion de b= %x",&b);
+    scanf("%c",&salir);
+}
