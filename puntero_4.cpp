a4c65d7171f86b1694588bb4732211b0f6bf4c19
Raquelcuartero <33655363+Raquelcuartero@users.noreply.github.com>
2018-01-25 11:22:30 +0100

    Add files via upload

--- puntero_4.cpp
+++ puntero_4.cpp
@@ -3,8 +3,8 @@
 #include<stdio.h>
 
 int main(){
-    int a=3;
-    int b=2;
+    float a=3;
+    float b=2;
     int salir;
     printf("direccion de a= %x\n",&a);
     printf("direccion de b= %x",&b);
