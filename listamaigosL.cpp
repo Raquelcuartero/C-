7bc88bb427fc8aba3d1dc1388e8a81eee9e33e0e
Raquelcuartero <33655363+Raquelcuartero@users.noreply.github.com>
2018-02-02 12:34:27 +0100

    Add files via upload

--- listamaigosL.cpp
+++ listamaigosL.cpp
@@ -1,33 +1,22 @@
 #include<stdio.h>
-#include<sydlib.h>
+#include<stdlib.h>
+#include<string.h>
 int main(){
     int salir;
     char *lista[5];//*punteros a letras
     char aux[15];
     int cont;
-    for(cont=0,cont<5;cont++){
-        pritf("\nNombre amigo %i: ",cont) // %i de valor
+    int l;
+    for(cont=0;cont<5;cont++){
+        printf("\nNombre amigo %i: ",cont); // %i de valor, lo que viene son los amigos luego
         scanf("%s",aux);  //aux vector de 15 letras donde se guarda provisionalmente el nombre del amigo    
-        l=medida(aux);
-        lista[cont]=malloc(l*sizeof(char));                
-    
-    
+        l=15;//l=medida(aux)
+        lista[cont]=(char *) malloc(l*sizeof(char));                
+        strcpy(lista[cont],aux); //te lo copia entero stringcopy
     }                      
-    lista[0]=(char *) malloc(8*sizeof(char));//1 letra ocupa 1 bytes. Se necesitan 8 bytes para la palabra
-    lista[1]=(char *) malloc(7*sizeof(char));
-    lista[2]=(char *) malloc(9*sizeof(char));
-    printf("\nPrimer rey mago: ");
-    scanf("%s",lista[0]); //scanf equivalente a cin. Cuando el ordenador lee scanf va a la memoria del ordenador
-    printf("\nSegundo rey mago: ");
-    scanf("%s",lista[1]);
-    printf("\nTercer rey mago: ");
-    scanf("%s",lista[2]);
-    
-    printf("\nLos tres Reyes magos son: ");
-    printf("%s, ",lista[0]);  //%s es string
-    printf("%s y ",lista[1]);
-    printf("%s,",lista[2]);
-    
+    for(cont=0;cont<5;cont++){
+        printf("\n%s",lista[cont]);
+    }
     printf("\nToca cualquier letra: ");
     scanf("%i",&salir);
 }
