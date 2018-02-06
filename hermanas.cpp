748c9f2b6b7c7fde999af065ede461f2a3ce3756
Raquelcuartero <33655363+Raquelcuartero@users.noreply.github.com>
2018-02-06 11:16:21 +0100

    Add files via upload

---
+++ hermanas.cpp
@@ -0,0 +1,10 @@
+//Programas sobre el manejo de memoria
+#include<stdio.h>
+
+int main(){
+    int hermanos=3;
+    int hermanas=2;
+    int salir;
+    printf("Tengo %i hermanas y %i hermanos",hermanas,hermanos);
+    scanf("%c",&salir);
+}
--- listamaigosL.cpp
+++ listamaigosL.cpp
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+int medida(char aux[]){
+    int l=0;
+    while(aux[l]!='\0'){
+          l++;
+    }
+    return l;
+}
+
 int main(){
     int salir;
     char *lista[5];//*punteros a letras
@@ -10,7 +18,8 @@ int main(){
     for(cont=0;cont<5;cont++){
         printf("\nNombre amigo %i: ",cont); // %i de valor, lo que viene son los amigos luego
         scanf("%s",aux);  //aux vector de 15 letras donde se guarda provisionalmente el nombre del amigo    
-        l=15;//l=medida(aux)
+        l=medida(aux);
+        printf(" %i\n",l);
         lista[cont]=(char *) malloc(l*sizeof(char));                
         strcpy(lista[cont],aux); //te lo copia entero stringcopy
     }                      
