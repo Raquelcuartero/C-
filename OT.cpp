091e0d4c3742e12423ee2b4e9e8b3c955a1a75d0
Raquelcuartero <33655363+Raquelcuartero@users.noreply.github.com>
2018-02-15 11:56:35 +0100

    Add files via upload

---
+++ OT.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<fstream>
+int posicion; //definimos una variable llamada  posicion para guardar un numero entero
+char *ganadores[3]; // vector de 3 punteros que apuntan a 3 variables char 
+for(posicion=0;osicion<3;posicion++);{
+    ganadores[posicion]=(char *) malloc(10*sizeof(char)); //reservamos memoria =malloc para guardar 10 bloques el tamaño de una letra, busca un hueco de 10 posiciones en cada una una letra
+}
+strcpy(ganadores[0],"Amaia"); // manera rapida para copiar en la derecha la dirección de la izquierda
+strcpy(ganadores[1],"Aitana");
+strcpy(ganadores[2],"Miriam");
+
